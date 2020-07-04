d283ea52fc2ff689cad9cd5970629e684dfcc9ce
MkshSahani <msco18333@gmail.com>
2020-07-05 00:56:05 +0530

    Stack.h updated

--- stack.h
+++ stack.h
@@ -1,23 +1,24 @@
 #include <iostream>
-#ifndef STACK_H 
-#define STACK_H 
+#ifndef STACK_H
+#define STACK_H
 
 using namespace std;
 
 template <typename T>
-class Stack; 
+class Stack;
 
 template <typename T>
-class Node 
+class Node
 {
 public:
     Node();
     Node(T key);
+
 private:
     T data;
     Node<T> *next;
 
-friend class Stack<T>;
+    friend class Stack<T>;
 };
 
 template <typename T>
@@ -34,32 +35,33 @@ Node<T>::Node(T key)
 }
 
 template <typename T>
-class Stack 
+class Stack
 {
 public:
     Stack();
     void push(T key);
-    Node<T>* pop();
+    Node<T> *pop();
     bool isempty();
     void print_stack();
+
 private:
     Node<T> *top;
 };
 
 template <typename T>
-List<T>::List()
+Stack<T>::Stack()
 {
     top = NULL;
 }
 
 template <typename T>
-void List<T>::push(T key)
+void Stack<T>::push(T key)
 {
-    if(this->top == NULL)
+    if (this->top == NULL)
     {
         this->top = new Node<T>(key);
     }
-    else 
+    else
     {
         Node<T> *new_node = new Node<T>(key);
         new_node->next = this->top;
@@ -68,27 +70,26 @@ void List<T>::push(T key)
 }
 
 template <typename T>
-Node<T>* List<T>::pop()
+Node<T> *Stack<T>::pop()
 {
-    Node<T>* ret = this->top;
-    Node<T>* temp = this->top;
+    Node<T> *ret = this->top;
+    Node<T> *temp = this->top;
     this->top = this->top->next;
     delete temp;
     return ret;
 }
 
 template <typename T>
-void List<T>::print_stack()
+void Stack<T>::print_stack()
 {
-    Node<T> *current = this->top; 
+    Node<T> *current = this->top;
     cout << "{ Stack : ";
-    while(current != NULL)
+    while (current != NULL)
     {
         cout << current->data << ", " << endl;
         current = current->next;
     }
     cout << " }" << endl;
 }
-
 
 #endif
